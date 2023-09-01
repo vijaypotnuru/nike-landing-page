8a8b4ee06604de847e096df5f9986374f95d534d
Durga Potnuru <vijaypotnuru123@gmail.com>
2023-09-01 19:26:54 +0530

    nav-bar added

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import {
   Subscribe,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    {/* <Nav/> */}
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,28 @@
+import Button from "../components/Button";
+
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2"
+    >
+      <div className="relative xl:w2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          you active life.
+        </p>
+        <Button label="Show now" iconUrl={arrowRight} />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
