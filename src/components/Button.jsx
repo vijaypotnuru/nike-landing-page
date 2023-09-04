064ae2f7d23bf0fa895ea96ee2ebf23681ebf07e
Durga Potnuru <vijaypotnuru123@gmail.com>
2023-09-04 18:22:54 +0530

    SuperQuality Section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,32 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
-      type="button"
-      className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
-      <img
-        src={iconUrl}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
+        />
+      )}
     </button>
   );
 };
 
 export default Button;
+  
