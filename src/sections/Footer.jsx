36c3a9a3ccdaa30fd1fab43169222a44f57f1143
Durga Potnuru <vijaypotnuru123@gmail.com>
2023-09-08 15:05:01 +0530

    some changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
           </p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+              <div key={icon.alt} className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
             ))}
